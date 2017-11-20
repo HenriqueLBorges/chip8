c9f13571a5e2c3d8cbcc39b97628e3defb465446
Henrique Borges <henriquelb96@gmail.com>
2017-11-20 18:07:34 -0200

    changes

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 var fs = require("fs");
-var chip8 = require("./chip8");
-var readStream = fs.createReadStream("PONG2");
-readStream.on("data", function (chunk) {
+//var chip8 = require("./static/scripts/chip8");
+//var readStream = fs.createReadStream("./Roms/PONG2");
+var express = require('express');
+var app = express();
+
+/*readStream.on("data", function (chunk) {
     chip8.reset();
     chip8.loadRom(chunk);
-    ctx.clear();
     while (true) {
         chip8.emulateCycle();
     }
+});*/
+
+//Set the view engine to EJSs
+app.set('view engine', 'ejs');
+
+//Set the available port or 3000
+var port = process.env.Port || 3000;
+
+//Set the static folder
+app.use(express.static(__dirname + '/static'));
+
+//Chip 8 Roms
+var roms = [
+    { name: 'PONG2'},
+    { name: 'SPACE INVADERS'},
+];
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        roms: roms
+    });
 });
+
+app.listen(port);
